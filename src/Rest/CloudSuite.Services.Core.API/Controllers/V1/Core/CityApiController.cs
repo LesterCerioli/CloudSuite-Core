002f7aea49e2b480f5b55132f8f2d25d2ddf62a2
LesterCerioli <lesterlucasit@hotmail.com>
2024-04-04 06:37:17 -0300

    feat(#1267): Building a basic structure API

---
+++ src/Rest/CloudSuite.Services.Core.API/Controllers/V1/Core/CityApiController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CloudSuite.Services.Core.Api.Controllers.V1.Core
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CityApiController : ControllerBase
+    {
+        // GET: api/<CityApiController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<CityApiController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CityApiController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<CityApiController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CityApiController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
