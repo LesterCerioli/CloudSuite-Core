d666f45df2a46608d9a7753bba72e452f7408b2f
LesterCerioli <lesterlucasit@hotmail.com>
2024-04-04 21:09:55 -0300

    feat(#1267): Buidling endpoints

--- src/Rest/CloudSuite.Services.Core.API/Controllers/V1/Core/AddressApiController.cs
+++ src/Rest/CloudSuite.Services.Core.API/Controllers/V1/Core/AddressApiController.cs
@@ -1,4 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CloudSuite.Modules.Application.Hadlers.Address;
+using CloudSuite.Modules.Application.Hadlers.Address.Responses;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Web.Http;
+using FromBodyAttribute = System.Web.Http.FromBodyAttribute;
+using HttpGetAttribute = System.Web.Http.HttpGetAttribute;
+using HttpPostAttribute = Microsoft.AspNetCore.Mvc.HttpPostAttribute;
+using RouteAttribute = System.Web.Http.RouteAttribute;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -8,36 +16,46 @@ namespace CloudSuite.Services.Core.Api.Controllers.V1.Core
 	[ApiController]
 	public class AddressApiController : ControllerBase
 	{
-		// GET: api/<AddressApiController>
-		[HttpGet]
+        private readonly ILogger<AddressApiController> _logger;
+        private readonly IMediator _mediator;
+
+        public AddressApiController(ILogger<AddressApiController> logger, IMediator mediator)
+		{
+			_logger = logger;
+			_mediator = mediator;
+		}
+
+
+		[AllowAnonymous]
+        [HttpPost("create")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Post([FromBody] CreateAddressCommand commandCreate)
+		{
+			var result = await _mediator.Send(commandCreate);
+            if (result.Errors.Any())
+            {
+                return BadRequest(result);
+            }
+            else
+            {
+                return Ok(result);
+            }
+        }
+
+        public async Task<IActionResult> AddressLineExists([FromBody] string addressLine)
+        {
+            var result = await _mediator.Send(new CheckAddressExistsByAddressLineResponse(addressLine));
+        }
+
+
+        // GET: api/<AddressApiController>
+        [HttpGet]
 		public IEnumerable<string> Get()
 		{
 			return new string[] { "value1", "value2" };
 		}
 
-		// GET api/<AddressApiController>/5
-		[HttpGet("{id}")]
-		public string Get(int id)
-		{
-			return "value";
-		}
-
-		// POST api/<AddressApiController>
-		[HttpPost]
-		public void Post([FromBody] string value)
-		{
-		}
-
-		// PUT api/<AddressApiController>/5
-		[HttpPut("{id}")]
-		public void Put(int id, [FromBody] string value)
-		{
-		}
-
-		// DELETE api/<AddressApiController>/5
-		[HttpDelete("{id}")]
-		public void Delete(int id)
-		{
-		}
+		
 	}
 }
