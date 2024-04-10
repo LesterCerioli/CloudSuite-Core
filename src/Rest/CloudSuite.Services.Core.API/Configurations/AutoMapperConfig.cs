8fb0336aa6532b45a766446435a2a9f216e9d240
matrix <lesterlucasit@hotmail.com>
2024-04-10 08:53:11 -0300

    feat(#1267): Improving Core Api

---
+++ src/Rest/CloudSuite.Services.Core.API/Configurations/AutoMapperConfig.cs
@@ -0,0 +1,13 @@
+namespace CloudSuite.Services.Core.API.Configurations
+{
+    public static class AutoMapperConfig
+    {
+        public static void AddAutoMapperConfiguration(this IServiceCollection services)
+		{
+			if (services == null) throw new ArgumentNullException(nameof(services));
+
+
+		}
+
+    }
+}
